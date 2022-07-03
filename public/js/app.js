19a8de9b4ade2274074b9745db6bb0482f31aa55
suresh varma <varma.suresh101@gmail.com>
2022-07-03 21:58:14 +0530

    removed unwanted

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,6 @@ messageThree.textContent=''
 messageFour.textContent=''
 
 // fetch('http://localhost:3000/weather?address=' + location).then((response)=>{we
-// 1223
 fetch('/weather?address=' + location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
